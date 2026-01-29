bf2e5641d031adcb4bc7afe11227b4a4d0f418cd
starrison17 <samuelarthurstarrison3017@gmail.com>
2026-01-29 16:30:49 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react()
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  plugins: [react()],
+  base: '/nes-election-system/'
+})
